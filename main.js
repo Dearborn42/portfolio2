9a4918f5410ff15dc78961bbc1c670b8a08346ce
Dearborn42 <amurf26@outlook.com>
2023-04-21 16:10:19 -0700

    finishing up animation

--- main.js
+++ main.js
@@ -6,9 +6,9 @@ $(window).on('load', function(){
 
 $(".carousel-control-next").on('click', function(){
         if (!$('#info').hasClass('active')) {
-                $('#box1').delay(1000).animate({width: '50%'}, 1000).animate({height: '100%'}, 1000);
-                $('#box2').delay(1000).animate({width: '50%'}, 1000).animate({height: '100%'}, 1000);
-                $('#box3').delay(1000).animate({width: '50%'}, 1000).animate({height: '100%'}, 1000);
-                $('#box4').delay(1000).animate({width: '50%'}, 1000).animate({height: '100%'}, 1000);
+                $('#box1').delay(1000).animate({width: '50%'}, 1000).animate({height: '100%'}, 1000).animate({backgroundColor: 'white'}, 1000);
+                $('#box2').delay(1000).animate({width: '50%'}, 1000).animate({height: '100%'}, 1000).animate({backgroundColor: 'white'}, 1000);
+                $('#box3').delay(1000).animate({width: '50%'}, 1000).animate({height: '100%'}, 1000).animate({backgroundColor: 'white'}, 1000);
+                $('#box4').delay(1000).animate({width: '50%'}, 1000).animate({height: '100%'}, 1000).animate({backgroundColor: 'white'}, 1000);
         }
     });
