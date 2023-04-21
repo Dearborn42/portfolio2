db1e154d75e956915e36ccd38512654d83088671
Dearborn42 <amurf26@outlook.com>
2023-04-21 15:22:04 -0700

    started to mess with home animation

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
 
 
-$(".carousel-control-next").on('click', function(){
-    if (!$('#info').hasClass('active')) {
-        console.log('The element has the class "my-class"');
-    }
-})
-
 $(window).on('load', function(){
     console.log( $(".bg-img").width() + " " +  $(".bg-img").height())
 })
+
+$(".carousel-control-next").on('click', function(){
+        if (!$('#info').hasClass('active')) {
+                $('#box1').delay(1000).animate({width: '50%'}, 1000).animate({height: '100%'}, 1000);
+                $('#box2').delay(1000).animate({width: '50%'}, 1000).animate({height: '100%'}, 1000);
+                $('#box3').delay(1000).animate({width: '50%'}, 1000).animate({height: '100%'}, 1000);
+                $('#box4').delay(1000).animate({width: '50%'}, 1000).animate({height: '100%'}, 1000);
+        }
+    });
