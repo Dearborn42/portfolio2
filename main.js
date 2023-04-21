3d489a5b97938ed95efc38314d5ed2920c475e68
Dearborn42 <amurf26@outlook.com>
2023-04-20 22:16:40 -0700

    will mess with home picture later

--- main.js
+++ main.js
@@ -5,3 +5,7 @@ $(".carousel-control-next").on('click', function(){
         console.log('The element has the class "my-class"');
     }
 })
+
+$(window).on('load', function(){
+    console.log( $(".bg-img").width() + " " +  $(".bg-img").height())
+})
