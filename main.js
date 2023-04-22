f0f80b4b34dfe1974d84ee154a8f45340f29a537
Dearborn42 <amurf26@outlook.com>
2023-04-21 18:22:41 -0700

    finished 3 boxes out of 4 on the home page

--- main.js
+++ main.js
@@ -4,11 +4,30 @@ $(window).on('load', function(){
     console.log( $(".bg-img").width() + " " +  $(".bg-img").height())
 })
 
+
+function animateBox(box){
+    $(box).delay(1000).animate({width: '50%'}, 1000).animate({height: '100%'}, 1000);
+    $('.toggle').delay(3500).fadeIn();
+}
+
 $(".carousel-control-next").on('click', function(){
         if (!$('#info').hasClass('active')) {
-                $('#box1').delay(1000).animate({width: '50%'}, 1000).animate({height: '100%'}, 1000).animate({backgroundColor: 'white'}, 1000);
-                $('#box2').delay(1000).animate({width: '50%'}, 1000).animate({height: '100%'}, 1000).animate({backgroundColor: 'white'}, 1000);
-                $('#box3').delay(1000).animate({width: '50%'}, 1000).animate({height: '100%'}, 1000).animate({backgroundColor: 'white'}, 1000);
-                $('#box4').delay(1000).animate({width: '50%'}, 1000).animate({height: '100%'}, 1000).animate({backgroundColor: 'white'}, 1000);
+                animateBox('#box1');
+                animateBox('#box2');
+                animateBox('#box3');
+                animateBox('#box4');
         }
     });
+
+$(document).ready(function(){
+  $('#collapseOne').collapse('hide');
+
+  $('#accordion1 .card-header').click(function() {
+    $('#collapseOne').collapse('toggle');
+  });
+  $('#collapseTwo').collapse('hide');
+
+  $('#accordion2 .card-header').click(function() {
+    $('#collapseTwo').collapse('toggle');
+  });
+});
