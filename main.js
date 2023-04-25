b5db20030870bcf6ea70239a3e0e1a372f53cd50
Dearborn42 <amurf26@outlook.com>
2023-04-25 16:53:37 -0700

    got rid of some unesscary function

--- main.js
+++ main.js
@@ -1,17 +1,8 @@
 
 
-$(window).on('load', function(){
-    console.log( $(".bg-img").width() + " " +  $(".bg-img").height())
-})
-
-
 function animateBox(box){
     $(box).delay(1000).animate({width: '50%'}, 1000).animate({height: '100%'}, 1000);
     $('.toggle').delay(3500).fadeIn();
-}
-function deanimateBox(box){
-  $(box).animate({width: '0%'}, 1).animate({height: '2px'}, 1);
-  $('.toggle').fadeOut();
 }
 
 $(".carousel-control-next").on('click', function(){
@@ -21,12 +12,6 @@ $(".carousel-control-next").on('click', function(){
           animateBox('#box2');
           animateBox('#box3');
           animateBox('#box4');
-        }else{
-          $(".divider").animate({height: '0%'}, 1);
-          deanimateBox('#box1');
-          deanimateBox('#box2');
-          deanimateBox('#box3');
-          deanimateBox('#box4');
         }
     });
 
