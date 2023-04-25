988cf824c6a7e8c378dc155512fc5b3c1c255a59
Dearborn42 <amurf26@outlook.com>
2023-04-24 18:52:59 -0700

    Update main.js

--- main.js
+++ main.js
@@ -9,14 +9,24 @@ function animateBox(box){
     $(box).delay(1000).animate({width: '50%'}, 1000).animate({height: '100%'}, 1000);
     $('.toggle').delay(3500).fadeIn();
 }
+function deanimateBox(box){
+  $(box).animate({width: '0%'}, 1).animate({height: '2px'}, 1);
+  $('.toggle').fadeOut();
+}
 
 $(".carousel-control-next").on('click', function(){
         if (!$('#info').hasClass('active')) {
           $(".divider").delay(1000).animate({height: '100%'}, 1000);
-                animateBox('#box1');
-                animateBox('#box2');
-                animateBox('#box3');
-                animateBox('#box4');
+          animateBox('#box1');
+          animateBox('#box2');
+          animateBox('#box3');
+          animateBox('#box4');
+        }else{
+          $(".divider").animate({height: '0%'}, 1);
+          deanimateBox('#box1');
+          deanimateBox('#box2');
+          deanimateBox('#box3');
+          deanimateBox('#box4');
         }
     });
 
