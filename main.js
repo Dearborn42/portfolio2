dbd9fb7310bfdb9c80ebdc4f9453dd71ea38a694
Dearborn42 <amurf26@outlook.com>
2023-04-25 18:23:00 -0700

    changing the slide functions

--- main.js
+++ main.js
@@ -5,15 +5,44 @@ function animateBox(box){
     $('.toggle').delay(3500).fadeIn();
 }
 
+var i = 0;
+
+// Slide to the next item
+function slideNext() {
+  if (i != 1) {
+    i++;
+    $('#carouselExample').carousel(1);
+  } else {
+    i--;
+    $('#carouselExample').carousel(0);
+  }
+}
+
+// Slide to the previous item
+function slidePrev() {
+  if (i != 0) {
+    $('#carouselExample').carousel('prev');
+    i--;
+  } else {
+    $('#carouselExample').carousel('next');
+    i++;
+  }
+}
+
+// Bind the slideNext and slidePrev functions to the next and previous buttons
+
+$('.carousel-control-prev').click(slidePrev);
+
 $(".carousel-control-next").on('click', function(){
-        if (!$('#info').hasClass('active')) {
-          $(".divider").delay(1000).animate({height: '100%'}, 1000);
-          animateBox('#box1');
-          animateBox('#box2');
-          animateBox('#box3');
-          animateBox('#box4');
-        }
-    });
+    slideNext();
+    if (!$('#info').hasClass('active')) {
+        $(".divider").delay(1000).animate({height: '100%'}, 1000);
+        animateBox('#box1');
+        animateBox('#box2');
+        animateBox('#box3');
+        animateBox('#box4');
+    }
+});
 
 $(document).ready(function(){
   $('#collapseOne').collapse('hide');
