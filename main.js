51df371b965df3ff67b26418e17911e9eeb98148
Dearborn42 <amurf26@outlook.com>
2023-04-26 21:55:11 -0700

    working on email function

--- main.js
+++ main.js
@@ -28,3 +28,31 @@ $(document).ready(function(){
     $('#collapseTwo').collapse('toggle');
   });
 });
+
+
+$('.send-email').on('click', function(){
+  let subject = $(".Subject").val();
+  let email = $('.email').val();
+  let password = $('.password').val();
+  let text = $('.text').val();
+
+  Email.send({
+    Host: "smtp.gmail.com",
+    Username: `${email}`,
+    Password: `${password}`,
+    To: "amurf26@outlook.com",
+    From: `${email}`,
+    Subject: `${subject}`,
+    Body: `${text}`,
+    Port: 587,
+    TLS: {
+      ciphers: "SSLv3",
+    },
+})
+  .then(function (message) {
+    alert("Email sent successfully");
+  })
+  .catch(function (error) {
+    alert("Error sending email: " + error);
+  });
+})
